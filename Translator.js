d286088f1153ea56cc41462415246a7c6c2fc624
Ajay Kumar R <ajaykumarr3024@gmail.com>
2020-11-16 00:29:53 +0530

    Languages working

--- Translator.js
+++ Translator.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { View, TextInput, StyleSheet, TouchableOpacity, TouchableHighlight, Text, Picker, Image } from 'react-native';
-import { PowerTranslator, ProviderTypes, TranslatorConfiguration, TranslatorFactory } from 'react-native-power-translator';
+import { View, TextInput, StyleSheet,Keyboard, TouchableOpacity, TouchableHighlight, Text, Picker, Image } from 'react-native';
+
 import Languages from './Languages.json';
 
 export default class Translator extends Component {
@@ -14,45 +14,90 @@ export default class Translator extends Component {
            inputText: "",
            outputText: "",
            submit: false,
+           output: ''
        };
    }
 
    render() {
-    TranslatorConfiguration.setConfig(ProviderTypes.Google, 'XXXX', this.state.languageCode,this.state.languageTo );
        return (
            <View style = {styles.container}>
+               <Text style={{textAlign: "center"}}>Language Translator</Text>
+
+               <View>
+                    <Text style={{padding: 15}}>
+                        Select the language to be translated from:
+                    </Text>
+                </View>
+                <View style={{padding:10}}>
+                    <Picker
+                    selectedValue={this.state.languageFrom}
+                    onValueChange={ lang => this.setState({languageFrom: lang})}
+                    >
+                        {Object.keys(Languages).map(key => (
+                            <Picker.Item label={Languages[key]} value={key} />
+                        ))}
+                    </Picker>
+               </View>
+
                <View style={styles.input}>
                    <TextInput
-                       style={{flex:1, height: 80}}
-                       placeholder="Enter Text"
+                       style={{flex:1, height: 80, padding: 10}}
+                       placeholder="Enter Text to be translated from"
                        underlineColorAndroid="transparent"
                        onChangeText = {inputText => this.setState({inputText})}
                        value={this.state.inputText}
                    />
                </View>
 
-               <Picker
-               selectedValue={this.state.languageTo}
-               onValueChange={ lang => this.setState({languageTo: lang, languageCode: lang})}
-               >
-                   {Object.keys(Languages).map(key => (
-                       <Picker.Item label={Languages[key]} value={key} />
-                   ))}
-               </Picker>
-
-               <View style = {styles.output}>
-                  {/* output text displays here.. */}
-                  {this.state.submit && <PowerTranslator  text={this.state.inputText} />}
+                <View>
+                    <Text style={{padding: 15}}>
+                        Select the language to be translated to:
+                    </Text>
+                </View>
+                <View style={{padding:10}}>
+                    <Picker
+                    selectedValue={this.state.languageTo}
+                    onValueChange={ lang => this.setState({languageTo: lang, languageCode: lang})}
+                    >
+                        {Object.keys(Languages).map(key => (
+                            <Picker.Item label={Languages[key]} value={key} />
+                        ))}
+                    </Picker>
                </View>
+
+               
                <TouchableOpacity
                    style = {styles.submitButton}
-                   onPress = {this.handleTranslate}
+                   onPress = {() => {
+                    fetch('https://translation.googleapis.com/language/translate/v2?key=AIzaSyCvfXHDEUTzb9wwtgHWiWpcrTpl6ewtsvA&target=' + this.state.languageTo + '&source=' + this.state.languageFrom + '&format=text&q=' +this.state.inputText )
+                    .then(response => response.json())
+                    .then(json => {
+                    // console.log(json)
+                    this.setState({
+                    outputText:json.data.translations[0].translatedText
+                    })
+                    })
+                    }}
                >
                    <Text style = {styles.submitButtonText}> Submit </Text>
                </TouchableOpacity>
+
+               
+                
+                <View>
+                    <Text style={{marginLeft: 10}}>
+                        Translated text:
+                    </Text>
+                </View>
+               <View style = {styles.output}>
+                  <Text>
+                  {this.state.outputText}
+                  </Text>
+               </View>
            </View>
        )
    }
+   
 }
 
 const styles = StyleSheet.create({
@@ -67,8 +112,8 @@ const styles = StyleSheet.create({
        borderWidth: .5,
        borderColor: '#000',
        // height: 40,
-       borderRadius: 5 ,
-       margin: 10
+       borderRadius: 6 ,
+       margin: 12
    },
    output: {
        flexDirection: 'row',
@@ -82,13 +127,15 @@ const styles = StyleSheet.create({
        height: 80,
    },
    submitButton: {
-       backgroundColor: '#7a42f4',
+       backgroundColor: '#7c22b1',
        padding: 10,
        margin: 15,
        borderRadius: 5 ,
        height: 40,
    },
    submitButtonText:{
-       color: 'white'
+       color: 'white',
+       textAlign: 'center'
    },
 })
+
